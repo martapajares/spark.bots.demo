f6232481a4aab3ccfaed5462d3ce88b92faf4384
Marta Alexandra Pajares Gallego <marta.pajares@altran.es>
2018-03-01 15:12:20 +0100

    cloneroom3

--- skills/command-help.js
+++ skills/command-help.js
@@ -7,7 +7,7 @@ module.exports = function (controller) {
         var text = "Here are my skills:";
         text += "\n- " + bot.enrichCommand(message, ".commons") + ": shows metadata about myself";
         text += "\n- " + bot.enrichCommand(message, "help") + ": spreads the word about my skills";
-        text += "\n- " + bot.enrichCommand(message, "room") + ": copying the room";
+        text += "\n- " + bot.enrichCommand(message, "cloneroom") + ": copying the room";
         bot.reply(message, text);
     });
 }
